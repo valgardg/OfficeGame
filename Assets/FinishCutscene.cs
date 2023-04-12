b647b259707e9772229e057e03030eabd5f5c139
Arnþór Árni <70693270+Arnpor@users.noreply.github.com>
2023-04-12 15:45:54 +0000

    WebGL works now

--- Assets/FinishCutscene.cs
+++ Assets/FinishCutscene.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class FinishCutscene : MonoBehaviour
 {
 
     public SFXManager sfxManager;
-    public float cutsceneDuration;
+    public float cutsceneDuration = 13f;
     // Start is called before the first frame update
     void Start()
     {
+        VideoPlayer videoPlayer = GameObject.Find("Valgard_Anmaton").GetComponent<VideoPlayer>();
         sfxManager = GameObject.Find("SFXManager").GetComponent<SFXManager>();
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Valgard_Anmaton.mp4");
+        print(videoPlayer.url);
+        videoPlayer.Prepare();
+        videoPlayer.Play();
         StartCoroutine(WaitForCutscene());
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            sfxManager.stopOfficeSounds();
+            sfxManager.playMusic();
+            SceneManager.LoadScene("BobOffice");
+        }
     }
 
     // Update is called once per frame
@@ -23,3 +39,25 @@ public class FinishCutscene : MonoBehaviour
         SceneManager.LoadScene("BobOffice");
     }
 }
+
+/*
+    }
+
+    private void LateUpdate()
+    {
+        print(videoPlayer.frameCount - 1 == (ulong)videoPlayer.frame);
+        print(videoPlayer.frameCount - 1 + " / " + (ulong)videoPlayer.frame);
+        print(videoPlayer.isPlaying);
+        print(videoPlayer.isPaused);
+        if (videoPlayer.frameCount - 1 == (ulong)videoPlayer.frame && videoPlayer.isPlaying)
+        {
+            sfxManager.stopOfficeSounds();
+            sfxManager.playMusic();
+            SceneManager.LoadScene("BobOffice");
+        }
+    }
+}
+*/
+
+
+
